8f393807ec4a7138751f8ee707ae0f9f9121d619
Shintaro Onogi <shinyonogi@outlook.de>
2023-08-12 02:22:02 +0200

    deleting info.js

--- info.js
+++
@@ -1,7 +0,0 @@
-const info = {
-    "telegram_api_key": "6297584011:AAFY0goouGBoia552zI19fE4G0BFXAEqrfQ",
-    "open_ai_api_key" : "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"
-};
-
-module.exports = info;
-
