8a17769ffc2dbaad8f30b083117c310bfcd6b8bd
Shintaro Onogi <shinyonogi@outlook.de>
2023-08-12 04:01:10 +0200

    cat

--- message.js
+++ message.js
@@ -1,26 +1,29 @@
 const INFO = require('./info');
 const TOKEN = INFO.Telegram_TOKEN;
-const API_KEY = INFO.OPENAI_API_KEY;
+const SEND_MESSAGE_URL = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+const OPENAI_API_KEY = INFO.OPENAI_API_KEY;
+const OPENAI_URL = "https://api.openai.com/v1/engines/davinci-codex/completions";
 
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-    apiKey: API_KEY
+    apiKey: OPENAI_API_KEY
 });
 const openai = new OpenAIApi(configuration);
 
 const axios = require('axios');
 
-/*
-const chat_completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "Hello world" }],
-})
-*/
 
 const sendMessage = async (chat_id) => {
 
+    const chat_completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: "What is your name?" }],
+    });
+
+    const MESSAGE = chat_completion.data.choices[0].message.content;
+
     const CHAT_ID = chat_id;
-    const SEND_MESSAGE_URL = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 
     console.log("Sending Message...")
     console.log(`To ${CHAT_ID}`)
@@ -28,7 +31,7 @@ const sendMessage = async (chat_id) => {
     try {
         const response = await axios.post(SEND_MESSAGE_URL, {
         chat_id: CHAT_ID,
-        text: 'Hello World!'
+        text: MESSAGE
         });
 
         console.log('Message sent:', response.data);
