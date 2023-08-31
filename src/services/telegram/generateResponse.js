2ad5d129e213a09b108e48c6ef1d802cca94676e
Shintaro Onogi <shinyonogi@outlook.de>
2023-09-01 01:26:17 +0200

    MOVED typingResponse method to own file

--- src/services/telegram/generateResponse.js
+++ src/services/telegram/generateResponse.js
@@ -1,26 +1,16 @@
-const { axios, TOKEN } = require('../../configuration/config')
 const completion = require('../gpt/completion');
 const autoReply = require('../../botflow/autoreply');
-const fetchAllMessages =  require('../../database/fetchAllMessages')
+const fetchAllMessages =  require('../../database/fetchAllMessages');
+const typingResponse = require('./typingResponse');
 
 
 const generateResponse = async ( chatId, receivedMessage ) => {
     let replyBody;
 
-    axios.post(`https://api.telegram.org/bot${TOKEN}/sendChatAction`, {
-        chat_id: chatId,
-        action: 'typing'
-        })
-        .then(response => {
-        console.log('Typing action sent:', response.data);
-        })
-        .catch(error => {
-        console.error('Error sending typing action:', error);
-    });
+    typingResponse(chatId);
 
     let allMessages = await fetchAllMessages(chatId);
     allMessages.unshift({role : 'user', content : receivedMessage})
-
 
     let replyMessage = autoReply(chatId);
     const replyMessageEmpty = Object.keys(replyMessage).length === 0;
---
+++ src/services/telegram/typingResponse.js
@@ -0,0 +1,16 @@
+const { axios, TOKEN } = require('../../configuration/config');
+
+const typingResponse = ( chatId ) => {
+    axios.post(`https://api.telegram.org/bot${TOKEN}/sendChatAction`, {
+        chat_id: chatId,
+        action: 'typing'
+        })
+        .then(response => {
+        console.log('Typing action sent:', response.data);
+        })
+        .catch(error => {
+        console.error('Error sending typing action:', error);
+    });
+};
+
+module.exports = typingResponse;
